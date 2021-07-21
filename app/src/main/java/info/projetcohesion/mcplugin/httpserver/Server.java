21d11a4e2a9f1691f513d39477c22b2f26184c2c
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2021-07-21 23:19:27 +0200

    Implémentation d'un fichier de config pour le serveur HTTP.

--- app/src/main/java/info/projetcohesion/mcplugin/httpserver/Server.java
+++ app/src/main/java/info/projetcohesion/mcplugin/httpserver/Server.java
@@ -1,9 +1,11 @@
 package info.projetcohesion.mcplugin.httpserver;
 
 import com.sun.net.httpserver.HttpServer;
+import info.projetcohesion.mcplugin.managers.FileManager;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Objects;
 import java.util.concurrent.Executors;
 
 /**
@@ -11,6 +13,7 @@ import java.util.concurrent.Executors;
  */
 public class Server {
     private static HttpServer _server;
+    private static FileManager _config = new FileManager("http");
 
     /**
      * Start the integrated HTTP server
@@ -19,9 +22,11 @@ public class Server {
      */
     public static void start() throws IOException {
         if(_server == null) { // If the server is already running, do nothing
-            _server = HttpServer.create(new InetSocketAddress("localhost", 8080), 0); // TODO don't hardcode hostname and port
+            checkConfig();
+
+            _server = HttpServer.create(new InetSocketAddress(Objects.requireNonNull(_config.get().getString("server.hostname")), _config.get().getInt("server.port")), 0);
             _server.createContext("/", new Handler());
-            _server.setExecutor(Executors.newFixedThreadPool(10)); // TODO don't hardcode the thread count for performance reasons
+            _server.setExecutor(Executors.newFixedThreadPool(_config.get().getInt("perfs.threads")));
             _server.start();
 
             System.out.println("HTTP server started on " + _server.getAddress().toString());
@@ -38,5 +43,18 @@ public class Server {
             _server = null; // Stopped HttpServer objects can't be reused
             System.out.println("HTTP server stopped");
         }
+    }
+
+    private static void checkConfig() {
+        if(_config.get().getConfigurationSection("server") == null) {
+            _config.get().set("server.hostname", "localhost");
+            _config.get().set("server.port", 8080);
+        } if (_config.get().getConfigurationSection("perfs") == null) {
+            _config.get().set("perfs.threads", Runtime.getRuntime().availableProcessors());
+
+            System.out.println("Using all " + Runtime.getRuntime().availableProcessors() + " detected CPUs for HTTP traffic handling. You can change this in the config file.");
+        }
+
+        _config.save();
     }
 }
