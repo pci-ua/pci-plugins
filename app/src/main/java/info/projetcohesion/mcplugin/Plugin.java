4a687c3d036badb699d73cd3374bff0f9b8164ee
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2021-07-22 18:49:46 +0200

    Ajout d'un commentaire à Plugin.java

--- app/src/main/java/info/projetcohesion/mcplugin/Plugin.java
+++ app/src/main/java/info/projetcohesion/mcplugin/Plugin.java
@@ -4,6 +4,7 @@ import info.projetcohesion.mcplugin.commands.MainCommand;
 import info.projetcohesion.mcplugin.commands.MapArtCommand;
 import info.projetcohesion.mcplugin.events.PlayerChunkChangeEvent;
 import info.projetcohesion.mcplugin.httpserver.Server;
+import info.projetcohesion.mcplugin.managers.ImageStorageManager;
 import info.projetcohesion.mcplugin.utils.ImageMagick;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,7 +26,7 @@ public class Plugin extends JavaPlugin {
         registerCommands();
         registerEvents();
 
-        if(ImageMagick.isWorking()) {
+        if(ImageMagick.isWorking()) { // The HTTP server is useless without ImageMagick, so don't start it without it.
             try {
                 Server.start();
             } catch (IOException e) {
