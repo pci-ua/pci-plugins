b51ff164f166c60798f1b12a23660362760785c7
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2021-07-23 18:56:21 +0200

    Les maparts fonctionnent après un redémarrage. La correspondance entre les IDs des maps et les IDs des images est sauvegardé dans le fichier maps-id.yml

--- app/src/main/java/info/projetcohesion/mcplugin/events/MapInitEvent.java
+++ app/src/main/java/info/projetcohesion/mcplugin/events/MapInitEvent.java
@@ -1,6 +1,7 @@
 package info.projetcohesion.mcplugin.events;
 
 import info.projetcohesion.mcplugin.map.ImageMapRenderer;
+import info.projetcohesion.mcplugin.utils.FileUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -10,21 +11,30 @@ import org.bukkit.map.MapView;
 
 public class MapInitEvent implements Listener {
     private static String s_wipId;
+    private FileUtils idMap = new FileUtils("maps-id");
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onMapInit(MapInitializeEvent event) {
-        if(s_wipId != null) { // Normal map if s_wipId == null
-            MapView map = event.getMap();
-
-            // We don't want other renderers on the same map
-            for(MapRenderer r : map.getRenderers()) {
-                map.removeRenderer(r);
-            }
-
-            map.addRenderer(new ImageMapRenderer(s_wipId));
+        MapView map = event.getMap();
+        if(idMap.get().getString(Integer.toString(map.getId())) != null) {
+            this.render(map, idMap.get().getString(Integer.toString(map.getId())));
+        } else if(s_wipId != null) { // Normal map if s_wipId == null
+            this.render(map, s_wipId);
+            idMap.get().set(Integer.toString(map.getId()), s_wipId);
 
             s_wipId = null; // Set the WIP ID to null to indicate that the next map should be a vanilla one, except if a new command is issued in between
+
+            idMap.save();
         }
+    }
+
+    private void render(MapView map, String id) {
+        // We don't want other renderers on the same map
+        for(MapRenderer r : map.getRenderers()) {
+            map.removeRenderer(r);
+        }
+
+        map.addRenderer(new ImageMapRenderer(id));
     }
 
     /**
